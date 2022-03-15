8afdc7249df26f69b56c1c15aa08dd9a5babcdad
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-03-15 14:04:52 +0530

    basic add method

---
+++ index.js
@@ -0,0 +1,8 @@
+// javascript
+let x = 5
+let y = 10
+let sum = x + y
+
+document.getElementById('x').innerHTML= x
+document.getElementById('y').innerHTML= y
+document.getElementById('add').innerHTML= sum
